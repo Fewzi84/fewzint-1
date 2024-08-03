36980e5e407c11c61e0f21f31bfb5e0e16db6916
Fewzan <nejafewzi@gmail.com>
2024-08-03 17:27:19 +0300

    adding user to database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,28 @@ import AddPost from "@/components/AddPost";
 import Feed from "@/components/feed/Feed";
 import LeftMenu from "@/components/leftbar/LeftMenu";
 import RightMenu from "@/components/rightbar/RightMenu";
-export default function Home() {
+import { auth, currentUser } from "@clerk/nextjs/server";
+import prisma from "@/lib/client";
+
+const Home = async () => {
+  const signedUser = await currentUser();
+  if (!signedUser?.id) return;
+  const user = await prisma.user.findFirst({
+    where: {
+      id: signedUser.id,
+    },
+  });
+  if (!user) {
+    await prisma.user.create({
+      data: {
+        id: signedUser.id,
+        username: signedUser.username ? signedUser.username : "",
+        name: signedUser.firstName,
+        surname: signedUser.lastName,
+        avator: signedUser.imageUrl,
+      },
+    });
+  }
   return (
     <div className="flex gap-6 pt-6 min-h-[calc(100vh-96px)]">
       <div className="hidden xl:block w-[20%]">
@@ -21,4 +42,5 @@ export default function Home() {
       </div>
     </div>
   );
-}
+};
+export default Home;
